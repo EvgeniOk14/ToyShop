a020492c89e23e1ef4911ced7d2064d7e214625a
Evgeni.ok14 <evgeni.ok@mail.ru>
2023-06-14 19:34:53 +0300

    created a new class: createToy

---
+++ ToyShopProjectJava/src/Modul/CreateToy.java
@@ -0,0 +1,15 @@
+package Modul;
+
+public class CreateToy
+{
+    public void createToy()
+    {
+        Toy toy1 = new Toy(0, "Конструктор", 2, 2);
+        Toy toy2 = new Toy(0, "Робот", 4, 2);
+        Toy toy3 = new Toy(0, "Кукла", 2, 2);
+        Toy toy4 = new Toy(0, "Машинка", 6, 2);
+        Toy toy5 = new Toy(0, "Ракета", 1, 2);
+
+    }
+
+}
