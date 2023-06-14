c7aed7eef7fbf77000b804657d4b19dc281bc5aa
Evgeni.ok14 <evgeni.ok@mail.ru>
2023-06-14 19:45:10 +0300

    created a new class: WrightPrizeToFile

---
+++ ToyShopProjectJava/src/Modul/WritePrizeToFile.java
@@ -0,0 +1,19 @@
+package Modul;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class WritePrizeToFile
+{
+    void writePrizesToFile(List<Toy> prizes) {
+        try (FileWriter writer = new FileWriter("prizes.txt")) {
+            for (Toy toy : prizes) {
+                writer.write("Игрушка: " + toy.getName() + ", вес: " + toy.getWeight() + "\n");
+            }
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
