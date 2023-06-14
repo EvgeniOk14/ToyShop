fca187600ee8b6bff95d0f450278ea4528d75c8e
Evgeni.ok14 <evgeni.ok@mail.ru>
2023-06-14 19:42:11 +0300

    created a new class: UpdateWeight

---
+++ ToyShopProjectJava/src/Modul/UpdateWeight.java
@@ -0,0 +1,31 @@
+package Modul;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class UpdateWeight
+{
+    private int toyId;
+    private int weight;
+    private PriorityQueue<Toy> toyQueue;
+
+    public UpdateWeight(int toyId, int weight, PriorityQueue<Toy> toyQueue)
+    {
+        this.toyId = toyId;
+        this.weight = weight;
+        this.toyQueue = new PriorityQueue<>(Comparator.comparingInt(Toy::getWeight).reversed());
+    }
+
+    public void updateWeight(int toyId, int weight)
+    {
+        int encreasedWeight = Math.min(weight, 100);
+        for (Toy toy : toyQueue)
+        {
+            if (toy.getId() == toyId)
+            {
+                toy.setWeight(encreasedWeight);
+                break;
+            }
+        }
+    }
+}
